63ecb0c645edb6f7ae6e667be20bc2d5c0afaf62
Martin Klemm <martin.klemm@hs-offenburg.de>
2015-08-13 18:23:31 +0200

    Added measurement support

--- Modules/OpenIGTLink/mitkIGTLMessageProvider.h
+++ Modules/OpenIGTLink/mitkIGTLMessageProvider.h
@@ -85,6 +85,8 @@ namespace mitk {
     */
     itkGetMacro(StreamingTime, unsigned int);
 
+    virtual void Update() override;
+
   protected:
     IGTLMessageProvider();
     virtual ~IGTLMessageProvider();
@@ -177,6 +179,9 @@ namespace mitk {
 
     /** \brief flag to indicate if the provider is streaming */
     bool                                      m_IsStreaming;
+
+    /** Measurement class to calculate latency and frame count */
+    mitk::IGTLMeasurements m_Measurement;
 
   };
 
